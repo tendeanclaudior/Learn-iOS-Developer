687b4a3ee65204d599e43fe144a8f44b300b170b
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-05-15 16:01:52 +0800

    Initial Commit

---
+++ FormSwiftUI/FormSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormSwiftUIApp.swift
+//  FormSwiftUI
+//
+//  Created by Claudio Tendean on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FormSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
