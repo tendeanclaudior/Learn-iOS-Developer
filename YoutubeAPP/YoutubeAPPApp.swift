f3137ea3c3680715e9060689a0b966f5e75ffbc0
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-05-15 17:01:48 +0800

    Initial Commit

---
+++ YoutubeAPP/YoutubeAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeAPPApp.swift
+//  YoutubeAPP
+//
+//  Created by Claudio Tendean on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
