13daf8e934e73517a9cb54e4eeaff4632b309b5a
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-05-15 21:33:29 +0800

    Initial Commit

---
+++ ModelDataSwiftUI/ModelDataSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelDataSwiftUIApp.swift
+//  ModelDataSwiftUI
+//
+//  Created by Claudio Tendean on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ModelDataSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
